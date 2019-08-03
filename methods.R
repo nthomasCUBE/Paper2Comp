8d6cab8d93ec2e8ebedd2437e0c3a7cbb85ce85c
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-08-03 08:45:47 +0200

    Add files via upload

--- methods.R
+++ methods.R
@@ -19,7 +19,6 @@ cmp_papers=function(A1, A2, output, min_freq, genelist){
 		output1=pdf_text(A1)
 		output2=pdf_text(A2)
 		
-
 		docs=Corpus(VectorSource(output2[2:length(output2)]))
 		toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
 		docs <- tm_map(docs, toSpace, "/")
@@ -32,7 +31,8 @@ cmp_papers=function(A1, A2, output, min_freq, genelist){
 		m <- as.matrix(dtm)
 		v <- sort(rowSums(m),decreasing=TRUE)
 		d2 <- data.frame(word = names(v),freq=v)
-
+		print(d2[1:10,])
+		
 		docs=Corpus(VectorSource(output1[2:length(output1)]))
 		toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
 		docs <- tm_map(docs, toSpace, "/")
@@ -45,6 +45,7 @@ cmp_papers=function(A1, A2, output, min_freq, genelist){
 		m <- as.matrix(dtm)
 		v <- sort(rowSums(m),decreasing=TRUE)
 		d <- data.frame(word = names(v),freq=v)
+		print(d[1:10,])
 		
 		print(dim(d))
 		if(!is.null(genelist)){
@@ -54,11 +55,14 @@ cmp_papers=function(A1, A2, output, min_freq, genelist){
 		print(dim(d))
 
 		d=cbind(d,rep("black",dim(d)[1]))
-		d[d[,1]%in%d2[,1],3]="red"
 		colnames(d)[3]="color"
-		set.seed(1234)
-		wordcloud(words = d$word, freq = d$freq, min.freq = min_freq,
-			  max.words=200, random.order=FALSE, rot.per=0.35, 
+		d=subset(d,d[,2]>=min_freq)
+		d[toupper(d[,1])%in%toupper(d2[,1]),3]="red"
+		d=na.omit(d)
+		print(d)
+		#set.seed(1234)
+		wordcloud(words = d$word, scale=c(4,.5), freq = d$freq,
+			  max.words=Inf, min.freq=0, random.order=FALSE, ordered.colors=TRUE, rot.per=0.35, 
 			  colors=d$color)
 	})
 }
